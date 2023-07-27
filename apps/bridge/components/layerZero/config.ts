45ddb62874dc09097ae407d69e3be433bd34b17a
pectoraux <ekontetevi@yahoo.fr>
2023-07-27 12:53:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ apps/bridge/components/layerZero/config.ts
@@ -0,0 +1,13 @@
+const VERSION = '0.0.19'
+// https://unpkg.com/@layerzerolabs/x-pancakeswap-widget@0.0.19/element.mjs.sha384
+const SHA384 = 'vFsGllKrtj0a3Zmmbv9/oOUXwg5KRwOmB2Cxd82TA+iVK1rzBkC4RjQcg/tBjUrK'
+
+export const LAYER_ZERO_JS = {
+  src: `https://unpkg.com/@layerzerolabs/x-pancakeswap-widget@${VERSION}/element.mjs`,
+  css: `https://unpkg.com/@layerzerolabs/x-pancakeswap-widget@${VERSION}/element.css`,
+  integrity: `sha384-${SHA384}`,
+}
+
+export const PARTNER_ID = 0x0002
+export const FEE_COLLECTOR = '0x68C7ABB8b1c3D1cE467E28265770F3a7ECF32654'
+export const FEE_TENTH_BPS = '0'
