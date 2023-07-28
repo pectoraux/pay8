e7481c97bdd49c223219c943edfb172306135507
pectoraux <ekontetevi@gmail.com>
2023-07-28 19:40:22 +0000

    11

--- apps/web/src/pages/trustbounties/voting/[id].tsx
+++ apps/web/src/pages/trustbounties/voting/[id].tsx
@@ -1,63 +1,63 @@
-// // eslint-disable-next-line camelcase
-// import { SWRConfig, unstable_serialize } from 'swr'
-// import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
+// eslint-disable-next-line camelcase
+import { SWRConfig, unstable_serialize } from 'swr'
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
 // import { getLitigationSg } from 'state/trustbountiesvoting/helpers'
 // import Overview from 'views/TrustBountiesVoting/Proposal/Overview'
 
-// const ProposalPage = ({ fallback = {} }: InferGetStaticPropsType<typeof getStaticProps>) => {
-//   return (
-//     <SWRConfig
-//       value={{
-//         fallback,
-//       }}
-//     >
-//       <Overview />
-//     </SWRConfig>
-//   )
-// }
+const ProposalPage = ({ fallback = {} }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback,
+      }}
+    >
+      {/* <Overview /> */}
+    </SWRConfig>
+  )
+}
 
-// export const getStaticPaths: GetStaticPaths = () => {
-//   return {
-//     fallback: true,
-//     paths: [],
-//   }
-// }
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    fallback: true,
+    paths: [],
+  }
+}
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const { id } = params
-//   if (typeof id !== 'string') {
-//     return {
-//       notFound: true,
-//     }
-//   }
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params
+  if (typeof id !== 'string') {
+    return {
+      notFound: true,
+    }
+  }
 
-//   try {
-//     const fetchedProposal = await getLitigationSg(id)
-//     if (!fetchedProposal) {
-//       return {
-//         notFound: true,
-//         revalidate: 1,
-//       }
-//     }
-//     return {
-//       props: {
-//         fallback: {
-//           [unstable_serialize(['litigation-trustbounties', id])]: fetchedProposal,
-//         },
-//       },
-//       revalidate:
-//         fetchedProposal.active === false
-//           ? 60 * 60 * 12 // 12 hour
-//           : 3,
-//     }
-//   } catch (error) {
-//     return {
-//       props: {
-//         fallback: {},
-//       },
-//       revalidate: 60,
-//     }
-//   }
-// }
+  try {
+    const fetchedProposal = {} // await getLitigationSg(id)
+    if (!fetchedProposal) {
+      return {
+        notFound: true,
+        revalidate: 1,
+      }
+    }
+    return {
+      props: {
+        fallback: {
+          [unstable_serialize(['litigation-trustbounties', id])]: fetchedProposal,
+        },
+      },
+      revalidate: 3,
+      // fetchedProposal?.active === false
+      //   ? 60 * 60 * 12 // 12 hour
+      //   : 3,
+    }
+  } catch (error) {
+    return {
+      props: {
+        fallback: {},
+      },
+      revalidate: 60,
+    }
+  }
+}
 
-// export default ProposalPage
+export default ProposalPage
