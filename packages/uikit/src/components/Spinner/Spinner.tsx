c9d8a92f8dd87d044344e4fc768e70468126c71a
pectoraux <ekontetevi@gmail.com>
2023-08-24 22:18:37 +0000

    24

--- packages/uikit/src/components/Spinner/Spinner.tsx
+++ packages/uikit/src/components/Spinner/Spinner.tsx
@@ -6,7 +6,7 @@ import { Image } from "../Image";
 const Spinner: React.FC<React.PropsWithChildren<SpinnerProps>> = ({ size = 128 }) => {
   return (
     <Box width={size} height={size * 1.197} position="relative">
-      <Image width={size} height={size * 1.197} src="./dinausaur.gif" alt="leviathan-gif" />
+      <Image width={size} height={size * 1.197} src="/dinausaur.gif" alt="leviathan-gif" />
     </Box>
   );
 };
