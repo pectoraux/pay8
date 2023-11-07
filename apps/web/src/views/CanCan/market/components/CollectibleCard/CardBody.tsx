42ac72e4eceb469dc06107f1e6ddc3a4b5d109a7
pectoraux <ekontetevi@gmail.com>
2023-11-07 12:11:33 +0000

    8

--- apps/web/src/views/CanCan/market/components/CollectibleCard/CardBody.tsx
+++ apps/web/src/views/CanCan/market/components/CollectibleCard/CardBody.tsx
@@ -24,9 +24,9 @@ const StyledTimerText = styled(Heading)`
 
 const CollectibleCardBody: React.FC<any> = ({ nft, currentAskPrice, isUserNft }) => {
   const { t } = useTranslation()
-  const { tokenId: name } = nft
-  const bnbBusdPrice = useBNBBusdPrice()
-  const { mainCurrency } = useWorkspaceCurrency(nft?.ve?.toLowerCase(), nft?.tFIAT, nft?.usetFIAT, nft?.currentAskPrice)
+  // const { tokenId: name } = nft
+  // const bnbBusdPrice = useBNBBusdPrice()
+  // const { mainCurrency } = useWorkspaceCurrency(nft?.ve?.toLowerCase(), nft?.tFIAT, nft?.usetFIAT, nft?.currentAskPrice)
   // const isAuction = Number(nft?.bidDuration) > 0
   // const askOrder = useGetOrder(nft?.collection?.id, nft?.tokenId)?.data as any
   // const isDrop = parseInt(askOrder?.dropinTimer ?? '0')
@@ -37,40 +37,7 @@ const CollectibleCardBody: React.FC<any> = ({ nft, currentAskPrice, isUserNft })
   //   0,
   // )
   // const { days, hours, minutes } = getTimePeriods(diff)
-  return (
-    <CardBody
-      p="8px"
-      // style={{ background: isAuction ? 'linear-gradient(111.68deg, #F2ECF2 0%, #E8F2F6 100%)' : 'white' }}
-    >
-      ali
-      {/* <NFTMedia as={PreviewImage} nft={nft} height={320} width={320} mb="8px" borderRadius="8px" />
-      <Flex alignItems="center" justifyContent="space-between">
-        {nft?.tokenId && (
-          <Text fontSize="12px" color="textSubtle" mb="8px">
-            {nft?.tokenId}
-          </Text>
-        )}
-      </Flex>
-      <Text as="h4" fontWeight="600" mb="8px">
-        {getTitle(name)}
-      </Text>
-      <Box borderTop="1px solid" borderTopColor="cardBorder" pt="8px">
-        {currentAskPrice && (
-          <MetaRow title={isUserNft ? t('Your price') : t('Asking price')}>
-            <CostLabel cost={currentAskPrice} mainCurrency={mainCurrency} bnbBusdPrice={bnbBusdPrice} />
-          </MetaRow>
-        )}
-      </Box>
-      {isDrop && (days || hours || minutes) ? (
-        <>
-          <StyledTimerText pt="20px" pr="10px">
-            {t('Drops in')}
-          </StyledTimerText>
-          <Timer minutes={minutes} hours={hours} days={days} />
-        </>
-      ) : null} */}
-    </CardBody>
-  )
+  return <></>
 }
 
 export default CollectibleCardBody
