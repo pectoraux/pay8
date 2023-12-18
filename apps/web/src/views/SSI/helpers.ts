de3e56681b69e15dc36e83ffac273551809f697b
pectoraux <ekontetevi@gmail.com>
2023-12-18 23:03:41 +0000

    62

--- apps/web/src/views/SSI/helpers.ts
+++ apps/web/src/views/SSI/helpers.ts
@@ -63,7 +63,7 @@ export const filterProposalsByState = (proposals: any, state: any) => {
 }
 
 export const filterProposalsByQuery = (proposals: any, searchQuery: any, userDatas: any, entryType: any) => {
-  if (searchQuery && searchQuery?.length) {
+  if (searchQuery && searchQuery?.trim()?.length) {
     if (entryType !== EntryType.AUDITED) {
       const lowercaseQuery = latinise(searchQuery.toLowerCase())
       return proposals?.filter((proposal) => latinise(proposal?.question?.toLowerCase()).includes(lowercaseQuery))
