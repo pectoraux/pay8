d3e2f51cf9690eeef0485d9fc787750324560eff
pectoraux <ekontetevi@gmail.com>
2023-12-28 03:53:54 +0000

    77

--- apps/web/src/views/Ifos/SoonIfo.tsx
+++ apps/web/src/views/Ifos/SoonIfo.tsx
@@ -1,15 +1,6 @@
-import { bscTokens } from '@pancakeswap/tokens'
 import IfoContainer from './components/IfoContainer'
-import IfoSteps from './components/IfoSteps'
 import ComingSoonSection from './components/ComingSoonSection'
 
-const SoonIfo = () => (
-  <IfoContainer
-    ifoSection={<ComingSoonSection />}
-    ifoSteps={
-      <IfoSteps isLive={false} hasClaimed={false} isCommitted={false} ifoCurrencyAddress={bscTokens.cake.address} />
-    }
-  />
-)
+const SoonIfo = () => <IfoContainer ifoSection={<ComingSoonSection />} />
 
 export default SoonIfo
