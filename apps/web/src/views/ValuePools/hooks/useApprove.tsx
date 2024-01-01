5f845a1c8ffcb68e5a4e038b8eba6923a585678a
pectoraux <ekontetevi@gmail.com>
2024-01-01 00:25:58 +0000

    86

--- apps/web/src/views/ValuePools/hooks/useApprove.tsx
+++ apps/web/src/views/ValuePools/hooks/useApprove.tsx
@@ -1,14 +1,11 @@
 import { useCallback } from 'react'
-import { useAccount } from 'wagmi'
 import { MaxUint256 } from '@pancakeswap/swap-sdk-core'
-import { useAppDispatch } from 'state'
 import { useTranslation } from '@pancakeswap/localization'
 import { useERC20, useVaultPoolContract } from 'hooks/useContract'
 import { useToast } from '@pancakeswap/uikit'
 import { useCallWithGasPrice } from 'hooks/useCallWithGasPrice'
 import useCatchTxError from 'hooks/useCatchTxError'
 import { ToastDescriptionWithTx } from 'components/Toast'
-import { useActiveChainId } from 'hooks/useActiveChainId'
 import useCakeApprove from 'hooks/useCakeApprove'
 import useCakeApprovalStatus from 'hooks/useCakeApprovalStatus'
 
@@ -19,15 +16,13 @@ export const useApprovePool = (
   onSuccess = null,
 ) => {
   const { toastSuccess } = useToast()
-  const { chainId } = useActiveChainId()
   const { fetchWithCatchTxError, loading: pendingTx } = useCatchTxError()
   const { callWithGasPrice } = useCallWithGasPrice()
   const { t } = useTranslation()
-  const dispatch = useAppDispatch()
-  const { address: account } = useAccount()
 
   const handleApprove = useCallback(async () => {
     const receipt = await fetchWithCatchTxError(() => {
+      console.log('lpContract================>', lpContract, address)
       return callWithGasPrice(lpContract, 'approve', [address, MaxUint256])
     })
     if (receipt?.status) {
@@ -39,19 +34,7 @@ export const useApprovePool = (
       )
       onSuccess?.()
     }
-  }, [
-    chainId,
-    account,
-    dispatch,
-    onSuccess,
-    lpContract,
-    address,
-    earningTokenSymbol,
-    t,
-    toastSuccess,
-    callWithGasPrice,
-    fetchWithCatchTxError,
-  ])
+  }, [onSuccess, lpContract, address, earningTokenSymbol, t, toastSuccess, callWithGasPrice, fetchWithCatchTxError])
 
   return { handleApprove, pendingTx }
 }
